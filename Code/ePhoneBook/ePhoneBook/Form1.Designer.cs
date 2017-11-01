1570abbac9708d67b96fa82f24ba32a63660c597
Emran BatmanGhelich <EmranB@systemgroup.net>
2017-11-01 16:45:40 +0330

    First commit

---
+++ Code/ePhoneBook/ePhoneBook/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ePhoneBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            this.contactsLV = new System.Windows.Forms.ListView();
+            this.searchTB = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(13, 13);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(42, 13);
+            label1.TabIndex = 2;
+            label1.Text = "search:";
+            // 
+            // contactsLV
+            // 
+            this.contactsLV.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.contactsLV.Location = new System.Drawing.Point(12, 44);
+            this.contactsLV.Name = "contactsLV";
+            this.contactsLV.Size = new System.Drawing.Size(332, 245);
+            this.contactsLV.TabIndex = 0;
+            this.contactsLV.UseCompatibleStateImageBehavior = false;
+            // 
+            // searchTB
+            // 
+            this.searchTB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchTB.Location = new System.Drawing.Point(61, 12);
+            this.searchTB.Name = "searchTB";
+            this.searchTB.Size = new System.Drawing.Size(283, 20);
+            this.searchTB.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "New Contact";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 330);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.searchTB);
+            this.Controls.Add(this.contactsLV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView contactsLV;
+        private System.Windows.Forms.TextBox searchTB;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
