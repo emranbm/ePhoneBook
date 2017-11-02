c88cffe1874bef223d8de5b7360c81ebec27ba6b
Emran <EmranB@systemgroup.net>
2017-11-02 10:31:06 +0330

    Some boilerplates + Database

--- Code/ePhoneBook/ePhoneBook/MainForm.Designer.cs
+++ Code/ePhoneBook/ePhoneBook/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ePhoneBook
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,14 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.Label label1;
-            this.contactsLV = new System.Windows.Forms.ListView();
+            Telerik.WinControls.UI.ListViewDetailColumn listViewDetailColumn1 = new Telerik.WinControls.UI.ListViewDetailColumn("Column 0", "First Name");
+            Telerik.WinControls.UI.ListViewDetailColumn listViewDetailColumn2 = new Telerik.WinControls.UI.ListViewDetailColumn("Column 1", "Last Name");
+            Telerik.WinControls.UI.ListViewDetailColumn listViewDetailColumn3 = new Telerik.WinControls.UI.ListViewDetailColumn("Column 2", "Phone Number");
             this.searchTB = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.contactsLV = new Telerik.WinControls.UI.RadListView();
             label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.contactsLV)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -43,17 +47,6 @@
             label1.Size = new System.Drawing.Size(42, 13);
             label1.TabIndex = 2;
             label1.Text = "search:";
-            // 
-            // contactsLV
-            // 
-            this.contactsLV.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.contactsLV.Location = new System.Drawing.Point(12, 44);
-            this.contactsLV.Name = "contactsLV";
-            this.contactsLV.Size = new System.Drawing.Size(332, 245);
-            this.contactsLV.TabIndex = 0;
-            this.contactsLV.UseCompatibleStateImageBehavior = false;
             // 
             // searchTB
             // 
@@ -73,27 +66,43 @@
             this.button1.Text = "New Contact";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // contactsLV
+            // 
+            listViewDetailColumn1.HeaderText = "First Name";
+            listViewDetailColumn2.HeaderText = "Last Name";
+            listViewDetailColumn3.HeaderText = "Phone Number";
+            this.contactsLV.Columns.AddRange(new Telerik.WinControls.UI.ListViewDetailColumn[] {
+            listViewDetailColumn1,
+            listViewDetailColumn2,
+            listViewDetailColumn3});
+            this.contactsLV.Location = new System.Drawing.Point(12, 38);
+            this.contactsLV.Name = "contactsLV";
+            this.contactsLV.Size = new System.Drawing.Size(332, 251);
+            this.contactsLV.TabIndex = 4;
+            this.contactsLV.Text = "Some Texts";
+            this.contactsLV.ItemMouseDoubleClick += new Telerik.WinControls.UI.ListViewItemEventHandler(this.contactsLV_ItemMouseDoubleClick);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(356, 330);
+            this.Controls.Add(this.contactsLV);
             this.Controls.Add(this.button1);
             this.Controls.Add(label1);
             this.Controls.Add(this.searchTB);
-            this.Controls.Add(this.contactsLV);
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.contactsLV)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListView contactsLV;
         private System.Windows.Forms.TextBox searchTB;
         private System.Windows.Forms.Button button1;
+        private Telerik.WinControls.UI.RadListView contactsLV;
     }
 }
 
---
+++ Code/ePhoneBook/ePhoneBook/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telerik.WinControls.UI;
+
+namespace ePhoneBook
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            DatabaseEntities entities = new DatabaseEntities();
+            foreach (var contact in entities.Contacts)
+            {
+                contactsLV.Items.Add(contact);
+            }
+        }
+
+        private void contactsLV_ItemMouseDoubleClick(object sender, ListViewItemEventArgs e)
+        {
+            Contact contact = (Contact)e.Item.Value;
+            MessageBox.Show(contact.FirstName);
+        }
+    }
+}
